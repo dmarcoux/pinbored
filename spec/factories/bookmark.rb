741c6acc562dcb2aacde999960b1af2cbbe97f84
Dany Marcoux <dmarcoux@posteo.de>
2021-04-21 22:05:59 +0200

    Setup FactoryBot

---
+++ spec/factories/bookmark.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :bookmark do
+    # TODO: Set values
+    # title: string,
+    # url: text,
+    # description: text,
+    # visibility: integer
+  end
+end
